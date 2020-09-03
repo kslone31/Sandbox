f06ceb9a304876c5ed4f0ea24c9e379a4322c812
Kaden Slone <70672095+kslone31@users.noreply.github.com>
2020-09-02 20:39:31 -0400

    Add files via upload

---
+++ Sandbox/Point.java
@@ -0,0 +1,17 @@
+class Point{
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public double getx(){
+        return this.x;
+    }
+
+    public double gety(){
+        return this.y;
+    }
+}
